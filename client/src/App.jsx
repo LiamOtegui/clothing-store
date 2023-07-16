d97d5ddb8ae4bc471fe1ef9f6ae7184355daf62b
LiamOtegui <liamotegui@gmail.com>
2023-07-16 02:37:10 -0300

    back and front configuration finished

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-[5rem] text-blue-500'>
+        <h1>Testing</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
